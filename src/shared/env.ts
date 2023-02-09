b4be8cf7fefe428c21bb474e5f0e9dbf626fbda4
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-02-09 20:28:41 +0200

    feat: load .env variables

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -1,4 +1,20 @@
+import { config } from "dotenv";
+config();
 export const env = {
   videoOutFolder: "translated-videos",
   videoInputFolder: "videos",
+  backup: "building",
+  extension: {
+    audio: "mp3",
+    video: "mp4",
+  },
+  codec: {
+    audio: "libmp3lame",
+  },
+  deepGram: {
+    key: process.env.DEEPGRAM_KEY as string,
+  },
+  zensia: {
+    key: process.env.ZENSIA_KEY as string,
+  },
 };
