2ca9a308d5fd2b279dda508768c227f455f1e554
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-02-09 20:26:30 +0200

    feat: create setps service

---
+++ src/services/setps-servece.ts
@@ -0,0 +1,43 @@
+import { FfmpegCommand } from "fluent-ffmpeg";
+import { Ora } from "ora";
+import { serverError } from "~/shared/errors";
+
+type messageStepsService = {
+  initial: string;
+  connection: string;
+  start: string;
+  progress: string;
+  error: string;
+  end: string;
+};
+type oraType = Ora;
+
+type stepsServiceType = {
+  messageStep: messageStepsService;
+  ffmpegCommand: FfmpegCommand;
+  spinner: Ora;
+};
+export const stepsService = ({ messageStep, ffmpegCommand, spinner }: stepsServiceType): FfmpegCommand => {
+  ffmpegCommand.on("connection", () => {
+    spinner.text = messageStep.connection + `\n\n`;
+  });
+
+  ffmpegCommand.on("start", () => {
+    spinner.text = messageStep.start + `\n\n`;
+  });
+
+  ffmpegCommand.on("progress", (progress) => {
+    const percentage = progress.percent.toFixed(2);
+    spinner.text = `${messageStep.progress} ${percentage}%` + `\n\n`;
+  });
+
+  ffmpegCommand.on("error", (error) => {
+    console.error(messageStep.error);
+    serverError(error, "removeAudioFromVideo");
+  });
+
+  ffmpegCommand.on("end", () => {
+    spinner.succeed(messageStep.end + `\n\n`);
+  });
+  return ffmpegCommand;
+};
