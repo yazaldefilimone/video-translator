e236709999f5bf1e7e83b5179f46cf88d3e5aa1d
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-02-11 21:47:39 +0200

    feat: create domain events

---
+++ src/core/domain-events.ts
@@ -0,0 +1,29 @@
+import EventEmitter from "events";
+
+export const eventsNames = {
+  audio: {
+    removedAudio: "removed_audio",
+    removePaddingWorlds: "remove-padding_audio",
+    removedWorlds: "removed-padding_audio",
+    add: "add_audio",
+    covertWorldToAudio: "converted_world_audio",
+  },
+  video: {
+    add: "add_audio",
+  },
+  subtitle: {
+    write: "write_subtitle",
+    read: "read_subtitle",
+    toSRT: "words_to_srt",
+  },
+
+  translate: {
+    start: "start_translate",
+  },
+  errors: {
+    internalError: "server_error",
+    error: "app_error",
+  },
+};
+
+export const domainEvent = new EventEmitter();
