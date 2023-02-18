ae213e76b48049ec6068acb1e710498e4a8052fd
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-02-18 22:49:53 +0200

    feat: bootstrap

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -8,6 +8,7 @@ import {
   convertWorldsToAudio,
 } from "~/services/audio-service";
 import { convertWorldsToSrt } from "~/services/srt-service";
+import { convertSubtitleFileInLines } from "~/services/subrip-text-service";
 import { tranasteText } from "~/services/translate-service";
 
 import { removeAudioFromVideo, addAudioFromVideo } from "~/services/video-service";
@@ -18,13 +19,12 @@ import { domainEvent, eventsNames } from "./domain-events";
 
 export const bootstrap = async () => {
   const messages = message["pt"];
-  // removeAudioFromVideo({ messages, file: "/home/yazaldefilimone/www/learnspace/video-translator/videos/test.mp4" });
+  // removeAudioFromVideo({ messages, file: "/home/yazaldefilimone/www/learnspace/video-translator/videos/delba.mp4" });
   // domainEvent.on(eventsNames.audio.removedAudio, (file) => {
   //   removeWorldsFromAudioPadding({ file, language: "en" });
   // });
 
   // domainEvent.on(eventsNames.audio.removePaddingWorlds, (props) => {
-  //   terminal.yellow("Padding: \n");
   //   console.table({
   //     id: props?.id ?? "not found",
   //     status: props?.status ?? "not found",
@@ -32,17 +32,28 @@ export const bootstrap = async () => {
   // });
 
   (async () => {
-    let id = "rpiiscx2cd-ee62-4efe-997a-48f62b5a64aa";
+    const id = "rm25zern73-7d86-4b31-9831-9a13bdab99a8";
     const myPromise = (): Promise<any> => {
       return new Promise(async (resolve, reject) => {
         const verify = await assemblyVerifyTranscriptById(id);
         console.log({ verify });
         if (verify.status === "completed") {
           const subtitles = await assemblyRequestTranscriptAudioById(id);
+          const format = subtitles.sentences.map((entrie) => {
+            return {
+              type: "cue" as const,
+              data: {
+                start: entrie.start, // milliseconds
+                end: entrie.end,
+                text: entrie.text,
+              },
+            };
+          });
           await tranasteText({
-            words: subtitles.sentences,
+            words: format,
             language: "pt",
           });
+
           resolve(true);
         } else {
           reject(new Error("A resposta ainda não está pronta"));
@@ -76,16 +87,15 @@ export const bootstrap = async () => {
     });
   });
   domainEvent.on(eventsNames.subtitle.toSRT, (srt) => {
-    writeFileSync("/home/yazaldefilimone/www/learnspace/video-translator/building/test.srt", srt);
+    writeFileSync("/home/yazaldefilimone/www/learnspace/video-translator/building/delba.srt", srt);
     terminal.green("Arquio de legendas salvo!");
   });
   domainEvent.on(eventsNames.audio.covertWorldToAudio, async ({ file, worlds }) => {
     const w = worlds.map((w, i) => ({ [i]: w }));
-    console.log({ w });
     const log = await addAudioFromVideo({
-      videoFile: "/home/yazaldefilimone/www/learnspace/video-translator/videos/test.mp4",
+      videoFile: "/home/yazaldefilimone/www/learnspace/video-translator/videos/delba.mp4",
       audiosPath: file,
-      outVideo: "/home/yazaldefilimone/www/learnspace/video-translator/building/test.mp4",
+      outVideo: "/home/yazaldefilimone/www/learnspace/video-translator/building",
       times: w,
       messages,
     });
@@ -99,6 +109,26 @@ export const bootstrap = async () => {
     console.error("Ouve algum  error ");
     process.exit();
   });
+  domainEvent.on("finish", (props) => {
+    console.log({
+      status: "finish",
+      video: props,
+    });
+    process.exit();
+  });
 };
 
-bootstrap().then();
+bootstrap()
+  .then()
+  .catch((error) => console.log({ error }));
+
+// convertSubtitleFileInLines({
+//   file: "/home/yazaldefilimone/www/learnspace/video-translator/building/test.srt",
+// }).then((sub) => {
+//   tranasteText({
+//     language: "pt",
+//     words: sub,
+//   })
+//     .then(console.log)
+//     .catch(console.log);
+// });
