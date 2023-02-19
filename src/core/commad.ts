6deef9852c8228ef82c57a55286bd44b9965718d
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-02-19 21:47:16 +0200

    feat:  create all commands

--- src/core/commad.ts
+++ src/core/commad.ts
@@ -1,64 +1,178 @@
 import { terminal } from "terminal-kit";
-import { message } from "~/shared/messages";
 const serverError = "Internal Error, try again later";
 import { intro, outro, confirm, select, spinner, isCancel, cancel, text } from "@clack/prompts";
 import { setTimeout as sleep } from "node:timers/promises";
 import color from "picocolors";
-import { readdirSync } from "node:fs";
+import { existsSync, readdirSync } from "node:fs";
+import { writeFileSync } from "fs";
+import { setInterval } from "timers";
+import { assemblyVerifyTranscriptById } from "~/services/assembly-service";
+import {
+  removeWorldsFromAudioPadding,
+  removeWorldsFromAudioListem,
+  convertWorldsToAudio,
+} from "~/services/audio-service";
+import { convertWorldsToSrt } from "~/services/srt-service";
+import { convertSubtitleFileInLines } from "~/services/subrip-text-service";
+import { tranasteText } from "~/services/translate-service";
+
+import { removeAudioFromVideo, addAudioFromVideo } from "~/services/video-service";
+import { assemblyRequestTranscriptAudioById } from "~/settings/assembly";
+
+import { message } from "~/shared/messages";
+import { domainEvent, eventsNames } from "./domain-events";
+import ffmpeg from "fluent-ffmpeg";
+import path from "node:path";
 const rootPath = "/home/yazaldefilimone/www/learnspace/video-translator/videos";
 async function main() {
   console.log();
   intro(color.inverse("video translator"));
   const files = readdirSync(rootPath);
   const options = files.map((file) => ({ value: `${rootPath}/${file}`, label: file }));
-  const video = await select({
+  const file = await select({
     message: "Select video file in root folder:",
     options,
   });
-  console.log({ video });
-  const name = await text({
-    message: "What is your name?",
-    placeholder: "Anonymous",
-  });
-
-  if (isCancel(name)) {
-    cancel("Operation cancelled");
-    return process.exit(0);
-  }
-
-  const shouldContinue = await confirm({
-    message: "Do you want to continue?",
-  });
-
-  if (isCancel(shouldContinue)) {
-    cancel("Operation cancelled");
-    return process.exit(0);
-  }
-
-  const projectType = await select({
-    message: "Pick a project type.",
-    options: [
-      { value: "ts", label: "TypeScript" },
-      { value: "js", label: "JavaScript" },
-      { value: "coffee", label: "CoffeeScript", hint: "oh no" },
-    ],
-  });
-
-  if (isCancel(projectType)) {
-    cancel("Operation cancelled");
-    return process.exit(0);
+  if (isCancel(file)) {
+    process.exit();
   }
 
   const s = spinner();
-  s.start("Installing via npm");
+  const messages = message["pt"];
 
-  await sleep(3000);
+  s.start("Removendo Audio do Video...");
+  removeAudioFromVideo({ messages, file });
 
-  s.stop("Installed via npm");
+  domainEvent.on(eventsNames.audio.removedAudio, (file) => {
+    s.start("Processamento do audio do video...");
+    removeWorldsFromAudioPadding({ file, language: "en" });
+  });
+  domainEvent.on(eventsNames.audio.removePaddingWorlds, ({ id }) => {
+    // const id = "rm25zern73-7d86-4b31-9831-9a13bdab99a8";
+    const myPromise = (): Promise<any> => {
+      return new Promise(async (resolve, reject) => {
+        const verify = await assemblyVerifyTranscriptById(id);
+        if (verify.status === "completed") {
+          s.start("conversão do audio em texto...");
+          const subtitles = await assemblyRequestTranscriptAudioById(id);
+          const format = subtitles.sentences.map((entrie) => {
+            return {
+              type: "cue" as const,
+              data: {
+                start: entrie.start, // milliseconds
+                end: entrie.end,
+                text: entrie.text,
+              },
+            };
+          });
+          s.start("Tradução...");
+          await tranasteText({
+            words: format,
+            language: "pt",
+          });
 
-  outro("You're all set!");
+          resolve(true);
+        } else {
+          reject(new Error("A resposta ainda não está pronta"));
+        }
+      });
+    };
 
-  await sleep(1000);
+    const persistPromise = async () => {
+      while (true) {
+        try {
+          const response = await myPromise();
+          if (response) {
+            break;
+          }
+        } catch (error) {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+      }
+    };
+    persistPromise().then();
+  });
+
+  domainEvent.on(eventsNames.translate.end, async (words) => {
+    convertWorldsToSrt(words);
+    s.start("Conversão de texto em audio...");
+    await convertWorldsToAudio({
+      outPutFilename: "/home/yazaldefilimone/www/learnspace/video-translator/building/audios",
+      worlds: words,
+      language: "pt",
+    });
+  });
+  domainEvent.on(eventsNames.subtitle.toSRT, (srt) => {
+    writeFileSync("/home/yazaldefilimone/www/learnspace/video-translator/building/delba.srt", srt);
+    s.start("Salvando legendas...");
+  });
+  domainEvent.on(eventsNames.audio.covertWorldToAudio, async ({ file, worlds }) => {
+    const w = worlds.map((w, i) => w.data);
+    s.start("Salvando o video traduzindo...");
+    const log = await addAudioFromVideo({
+      videoFile: "/home/yazaldefilimone/www/learnspace/video-translator/videos/delba.mp4",
+      audiosPath: "/home/yazaldefilimone/www/learnspace/video-translator/building/audios",
+      outVideo: "/home/yazaldefilimone/www/learnspace/video-translator/building",
+      times: w,
+    });
+    s.stop("video traduzindo...");
+  });
+
+  // domainEvent.on("created", ({ outputPath }) => {
+  //   s.stop("Salvando...");
+  //   const command = ffmpeg();
+  //   command.input("/home/yazaldefilimone/www/learnspace/video-translator/videos/delba.mp4");
+  //   command.input("/home/yazaldefilimone/www/learnspace/video-translator/building/outputPath.mp3").seekInput(2.5);
+  //   command.videoCodec("copy");
+  //   command.audioCodec("libmp3lame");
+  //   command.outputOptions(["-map 0:v:0", "-map 1:a:0"]);
+  //   command.output("finish.mp4");
+  //   command.on("error", function (err) {
+  //     console.log("Erro ao alterar o áudio do vídeo: " + err.message);
+  //     process.exit();
+  //   });
+  //   command.on("end", () => {
+  //     domainEvent.emit("finish", `./finish.mp4`);
+  //   });
+  //   command.run();
+  // });
+  domainEvent.on(eventsNames.errors.error, () => {
+    console.error("Ouve algum  error ");
+    process.exit();
+  });
+  domainEvent.on(eventsNames.errors.internalError, () => {
+    console.error("Ouve algum  error ");
+    process.exit();
+  });
+  domainEvent.on("finish", (props) => {
+    console.log({
+      status: "finish",
+      video: props,
+    });
+    process.exit();
+  });
 }
 
-main().catch(console.error);
+main()
+  .then(() => {
+    domainEvent.on("created", ({ outputPath }) => {
+      const command = ffmpeg();
+      command
+        .input(path.join(__dirname, "../../videos/delba.mp4"))
+        .videoCodec("copy")
+        .input(outputPath)
+        .seekInput(2.5)
+        .audioCodec("libmp3lame")
+        .outputOptions(["-map 0:v:0", "-map 1:a:0"])
+        .output(path.join(__dirname, "../../finish.mp4"))
+        .on("error", function (err) {
+          console.log("Erro ao alterar o áudio do vídeo: " + err.message);
+          process.exit();
+        })
+        .on("end", () => {
+          domainEvent.emit("finish", `./finish.mp4`);
+        })
+        .run();
+    });
+  })
+  .catch(console.error);
